7afb10d581615801ef798ff7c7d25fbf2d91aa37
Casper Sondergaard <casper.sndergaard@gmail.com>
2022-04-05 18:13:26 +0000

    some changes

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,8 @@
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h2>Heading</h2>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps. Go. This works</p>
 </main>
 
 <style>
